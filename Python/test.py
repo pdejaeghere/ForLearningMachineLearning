28160e7c48726b8a6558499d1dc08e222e54e9e8
patrice De jaeghere <pdejaeghere@free.fr>
2026-02-27 16:18:14 +0100

    python , jupyter

--- Python/test.py
+++ Python/test.py
@@ -1,10 +1,48 @@
 import numpy as np
 
-def mysum(matrice):
-    return np.sum(matrice)
+classeur= {
+"positif":[],
+"negatif":[]
+}
+def trier(classeur,nombre):
+    if nombre<0:
+        classeur["negatif"].append(nombre);
+    else:
+        classeur["positif"].append(nombre);
+
+    return classeur
 
 
+def fibonaci(n):
+    a=0
+    b=1
+    
+    toreturn= [a]
+    while(b<n):        
+        a,b=b,a+b            
+        toreturn.append(a);
+    return toreturn;
+    
 
-A =np.ones((2,3))
-print(mysum(A))
 
+trier(classeur,10);
+trier(classeur,-1);
+trier(classeur,-6);
+
+print(classeur)
+
+for k,v in classeur.items():
+    print("key:",k,"value:",v)
+
+
+malist=["1","2","3"]
+dico={}
+dico=dico.fromkeys(malist,'v')
+print(dico)
+dico.pop("2")
+print(dico)
+dico["1"]="toto"
+print(dico)
+
+
+print(fibonaci(10))
